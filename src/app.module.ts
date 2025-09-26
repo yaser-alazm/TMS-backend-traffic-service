e9feea641fdec6eaed1e1e8a5df2999b1a5f6d1d
yaser-alazm <yaser.azem@gmail.com>
2025-09-26 07:01:29 +0200

    Add metrics functionality with MetricsController, MetricsMiddleware, and MetricsService; update AppModule and package.json to include prom-client dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health.controller';
+import { MetricsController } from './metrics.controller';
+import { MetricsMiddleware } from './metrics.middleware';
+import { MetricsService } from './metrics.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+  controllers: [AppController, HealthController, MetricsController],
+  providers: [AppService, MetricsMiddleware, MetricsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MetricsMiddleware).forRoutes('*');
+  }
+}
